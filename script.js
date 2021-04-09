1b8d180c39b2f7b463a15f2e4ff418a5e707ac21
Zeenat Parveen <zenizee563@gmail.com>
2021-04-09 14:54:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+const buttonValue = document.querySelectorAll("button")
+const screenValue = document.querySelector("#number");
+const answer = document.querySelector('#answer')
+let isOperator;
+let isdot;
+let isNumber;
+screenValue.value = '0';
+function refreshCalculator() {
+    isOperator = true;
+    isdot = false;
+    isNumber = true;
+    screenValue.value = "0"
+}
+refreshCalculator();
+buttonValue.forEach(element => {
+    element.addEventListener('click', e => {
+
+
+        let buttonText = e.target.innerText;
+
+        if (buttonText === "C") {
+            refreshCalculator();
+        }
+        else if (buttonText === '.' && !isdot) {
+            screenValue.value += buttonText;
+            isdot = true;
+        }
+
+        else if (buttonText === '+' && isOperator) {
+            if (screenValue.value != '0') {
+                screenValue.value += buttonText;
+            }
+            else {
+                screenValue.value = buttonText;
+            }
+            isOperator = false;
+            isNumber = true;
+
+        }
+        else if (buttonText === '-' && isOperator) {
+            if (screenValue.value != '0') {
+                screenValue.value += buttonText;
+            }
+            else {
+                screenValue.value = buttonText;
+            }
+            isOperator = false;
+            isNumber = true;
+        }
+        else if (buttonText === '*' && isOperator) {
+            if (screenValue.value != '0') {
+                screenValue.value += buttonText;
+                isOperator = false;
+                isNumber = true;
+            }
+
+        }
+        else if (buttonText === '/' && isOperator) {
+            if (screenValue.value != '0') {
+                screenValue.value += buttonText;
+                isOperator = false;
+                isNumber = true;
+            }
+
+        }
+        else if (buttonText === '%' && isOperator) {
+            if (screenValue.value != '0') {
+                screenValue.value += buttonText;
+                isOperator = false;
+                isNumber = true;
+            }
+
+        }
+
+
+        else if (buttonText >= '0' && buttonText <= 9 && isNumber) {
+            if (buttonText === '0' && screenValue.value === '0') {
+                isNumber = true;
+            }
+            else if (screenValue.value === '0') {
+                screenValue.value = buttonText;
+            }
+            else {
+                screenValue.value += buttonText;
+            }
+            isOperator = true;
+        }
+        else if (buttonText === '=') {
+            screenValue.value = eval(screenValue.value)
+
+            isNumber = false;
+        }
+
+    })
+
+
+
+
+
+
+
+});
